6b2e37c2ef1b3463dc57e7f34d6419710fa6fff0
rafe <raphael.lafrance@gmail.com>
2020-11-20 17:59:17 -0500

    Update tests

--- tests/setup.py
+++ tests/setup.py
@@ -5,12 +5,13 @@ from typing import Dict, List
 from traiter.pylib.util import clean_text
 
 from src.matchers.pipeline import Pipeline
-from src.pylib.util import TRANS
 
 TEST_PIPELINE = Pipeline()  # Singleton for testing
+
+TRANS = str.maketrans({'¼': '=', '⫻': '×', '#': '♂', '$': '♀'})
 
 
 def test_traits(text: str) -> List[Dict]:
     """Find entities in the doc."""
-    text = clean_text(text, trans=TRANS['custom'])
+    text = clean_text(text, trans=TRANS)
     return TEST_PIPELINE.test_traits(text)
