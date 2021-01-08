727fe308383ea55621fc772fc5e4d1f4fabcad4f
rafe <raphael.lafrance@gmail.com>
2021-01-08 15:53:51 -0500

    Use the new traiter matchers

--- src/matchers/pipeline.py
+++ src/matchers/pipeline.py
@@ -1,13 +1,25 @@
 """Create a trait pipeline."""
 
-from traiter.pylib.pipeline import SpacyPipeline
-# from traiter.pylib.sentencizer import SpacySentencizer
-from traiter.pylib.to_entities import ToEntities
+from traiter.pipeline import SpacyPipeline
+from traiter.rule_matcher import RuleMatcher
+from traiter.term_matcher import TermMatcher
+from traiter.to_entities import ToEntities
 
-# from .description_old import description
-from .matcher import Matcher
-from ..pylib.consts import ABBREVS, ATTACH_STEP, DESCRIPTION_STEP, GROUP_STEP, \
-    TRAIT_STEP
+from .body_part import BODY_PART
+from .body_part_count import BODY_PART_COUNT
+from .description import DESCRIPTION
+from .length import LENGTH
+from .max_width import MAX_WIDTH
+from .numeric import NUMERIC
+from .sci_name import SCI_NAME
+from .seta_count import SETA_COUNT
+from .size import SIZE
+from ..pylib.consts import ATTACH_STEP, DESCRIPTION_STEP, GROUP_STEP, NUMERIC_STEP, \
+    TERMS, TRAIT_STEP
+
+MATCHERS = [
+    BODY_PART, BODY_PART_COUNT, DESCRIPTION, LENGTH, MAX_WIDTH, NUMERIC,
+    SCI_NAME, SETA_COUNT, SIZE]
 
 
 class Pipeline(SpacyPipeline):
@@ -22,11 +34,10 @@ class Pipeline(SpacyPipeline):
         self.nlp.max_length *= 2
         self.nlp.disable_pipes(['ner'])
 
-        matcher = Matcher(self.nlp)
-        # sentencizer = SpacySentencizer(ABBREVS, headings='heading')
-        to_entities = ToEntities(self.entities2keep, self.token2entity)
-
-        # self.nlp.add_pipe(sentencizer, before='parser')
-        self.nlp.add_pipe(matcher, last=True)
-        self.nlp.add_pipe(to_entities, last=True)
-        # self.nlp.add_pipe(description, last=True)
+        TermMatcher.add_pipes(self.nlp, TERMS)
+        RuleMatcher.add_pipe(self.nlp, MATCHERS, NUMERIC_STEP)
+        RuleMatcher.add_pipe(self.nlp, MATCHERS, GROUP_STEP)
+        RuleMatcher.add_pipe(self.nlp, MATCHERS, TRAIT_STEP)
+        RuleMatcher.add_pipe(self.nlp, MATCHERS, ATTACH_STEP)
+        RuleMatcher.add_pipe(self.nlp, MATCHERS, DESCRIPTION_STEP)
+        ToEntities.add_pipe(self.nlp, self.entities2keep, self.token2entity)
