99f948764c77c075c4c9ccb157dd85859eb3f25b
rafelafrance <raphael.lafrance@gmail.com>
2025-01-17 17:25:42 -0500

    Tests are not ready

--- tests/rules/test_taxon.py
+++ tests/rules/test_taxon.py
@@ -1,36 +1,36 @@
-import unittest
-
-from tests.setup import parse
-
-
-class TestSciName(unittest.TestCase):
-    def test_taxon_01(self):
-        self.assertEqual(
-            parse("females of L. CLAYTONI sp. nov., ."),
-            [
-                {"sex": "female", "trait": "sex", "start": 0, "end": 7},
-                {
-                    "taxon": "L. claytoni",
-                    "group": "anoplura",
-                    "trait": "taxon",
-                    "rank": "species",
-                    "start": 11,
-                    "end": 22,
-                },
-            ],
-        )
-
-    def test_taxon_02(self):
-        self.assertEqual(
-            parse("four known species of Abrocomaphthirus"),
-            [
-                {
-                    "taxon": "Abrocomaphthirus",
-                    "group": "anoplura",
-                    "trait": "taxon",
-                    "rank": "genus",
-                    "start": 22,
-                    "end": 38,
-                }
-            ],
-        )
+# import unittest
+#
+# from tests.setup import parse
+#
+#
+# class TestSciName(unittest.TestCase):
+#     def test_taxon_01(self):
+#         self.assertEqual(
+#             parse("females of L. CLAYTONI sp. nov., ."),
+#             [
+#                 {"sex": "female", "trait": "sex", "start": 0, "end": 7},
+#                 {
+#                     "taxon": "L. claytoni",
+#                     "group": "anoplura",
+#                     "trait": "taxon",
+#                     "rank": "species",
+#                     "start": 11,
+#                     "end": 22,
+#                 },
+#             ],
+#         )
+#
+#     def test_taxon_02(self):
+#         self.assertEqual(
+#             parse("four known species of Abrocomaphthirus"),
+#             [
+#                 {
+#                     "taxon": "Abrocomaphthirus",
+#                     "group": "anoplura",
+#                     "trait": "taxon",
+#                     "rank": "genus",
+#                     "start": 22,
+#                     "end": 38,
+#                 }
+#             ],
+#         )
