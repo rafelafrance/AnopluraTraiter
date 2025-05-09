2250dfba9728f7de89e2a43f019007f36a5e583a
rafelafrance <raphael.lafrance@gmail.com>
2025-05-09 11:56:03 -0400

    Add missing module

--- tests/rules/test_part.py
+++ tests/rules/test_part.py
@@ -1,12 +1,12 @@
-# import unittest
-#
-# from anoplura.rules.part import Part
-# from tests.setup import parse
-#
-#
-# class TestBodyPart(unittest.TestCase):
-#     def test_part_01(self):
-#         self.assertEqual(
-#             parse("dorsal head suture"),
-#             [Part(part="dorsal head suture", start=0, end=18)],
-#         )
+import unittest
+
+from anoplura.rules.part import Part
+from tests.setup import parse
+
+
+class TestPart(unittest.TestCase):
+    def test_part_01(self):
+        self.assertEqual(
+            parse("Legs progressively larger"),
+            [Part(part="leg", start=0, end=4)],
+        )
