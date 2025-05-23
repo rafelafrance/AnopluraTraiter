5e546adab2838585693c47eb5a9caba918258d10
rafelafrance <raphael.lafrance@gmail.com>
2025-05-23 15:53:57 -0400

    Add missing files

---
+++ tests/rules/test_part_sclerotization.py
@@ -0,0 +1,19 @@
+import unittest
+
+from anoplura.rules.part_sclerotization import PartSclerotization
+from tests.setup import parse
+
+
+class TestSclerotized(unittest.TestCase):
+    def test_sclerotized_01(self):
+        self.assertEqual(
+            parse("Head, thorax, and abdomen moderately sclerotized"),
+            [
+                PartSclerotization(
+                    part=["head", "thorax", "abdomen"],
+                    amount_sclerotized="moderately",
+                    start=0,
+                    end=48,
+                )
+            ],
+        )
